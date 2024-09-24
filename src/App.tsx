2a0cb4471cc0efb61a80009a21b6e2bbc5a206b4
Rafael Teodosio <rafzteodosio@gmail.com>
2024-09-24 03:09:51 -0300

    Creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AppNavbar from './components/Navbar';
+import UserSearch from './pages/UserSearch';
+import RepoSearch from './pages/RepoSearch';
+import LandingPage from './components/LandingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Router>
+    <AppNavbar />
+    <Routes>
+      <Route path="/user-search" element={<UserSearch />} />
+      <Route path="/repo-search" element={<RepoSearch />} />
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
